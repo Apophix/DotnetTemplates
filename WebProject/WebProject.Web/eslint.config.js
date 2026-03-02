720b82df399c7a9266a5117244d7ab9521db8e11
Pat Howe <pat@falkon.net>
2026-03-02 10:53:52 -0600

    update eslint config to remove annoying rules

--- WebProject/WebProject.Web/eslint.config.js
+++ WebProject/WebProject.Web/eslint.config.js
@@ -8,8 +8,10 @@ export default [
 		rules: {
 			"@typescript-eslint/array-type": "off",
 			"no-unused-vars": "off",
-			"@typescript-eslint/no-explicit-any": "off", 
-			"@typescript-eslint/no-non-null-assertion": "off", 
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-non-null-assertion": "off",
+			"import/consistent-type-specifier-style": "off",
+			"sort-imports": "off",
 		},
 	},
 ];
